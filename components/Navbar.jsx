22021ecf9c61b2cd9162f83fdf3fa1695670c029
lucasnix <lucasniix@gmail.com>
2023-05-07 01:14:09 -0300

    Finish for upload II

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,24 +7,28 @@ import { navVariants } from '../utils/motion';
 
 const Navbar = () => (
   <motion.nav
-  variants={navVariants}
+    variants={navVariants}
     initial="hidden"
     whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
   >
     <div className="absolute w-[50%] inset-0 gradient-01" />
-    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+    <div
+      className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
+    >
       <img
         src="/search.svg"
         alt="search"
         className="w-[24px] h-[24px] object-contain"
       />
-      <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
-        LUCASDEV
-        </h2>
-        <h2>
-        <img src="/menu.svg" width="40" height="60" />
+      <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white">
+        Lucasdev - JavaScript Webdev
       </h2>
+      <img
+        src="/menu.svg"
+        alt="menu"
+        className="w-[24px] h-[24px] object-contain"
+      />
     </div>
   </motion.nav>
 );
